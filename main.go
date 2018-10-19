9801ac2fd4399d62ce49f0b11c3de20869db3d98
panndora <panndora@deadpig.cc>
2018-10-19 16:47:06 +0800

    好像稍微理解了模板如何传递两个以上变量的方法

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"regexp" //验证用户输入
 
-	_ "devmp/data"
+	_ "szsspwebdevmp/data"
 )
 
 // 路由
@@ -25,8 +25,64 @@ func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		queryPage(w, r)
 		return
 	}
+	if r.URL.Path == "/test" {
+		testPage(w, r)
+		return
+	}
 	http.NotFound(w, r)
 	return
+}
+
+func testPage(w http.ResponseWriter, r *http.Request) {
+
+	const tpl = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>{{.Title}}</title>
+    </head>
+    <body>
+        {{range .Items}}<div>{{ . }}</div>{{else}}<div><strong>no rows</strong></div>{{end}}
+    </body>
+</html>`
+
+	check := func(err error) {
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	// 创建一个新的模板，并且载入内容
+	t, err := template.New("webpage").Parse(tpl)
+	check(err)
+
+	// 定义传入到模板的数据，并在终端打印
+	data := struct {
+		Title string
+		Items []string
+	}{
+		Title: "My page",
+		Items: []string{
+			"My photos",
+			"My blog",
+			"My superman",
+			"My test",
+		},
+	}
+	err = t.Execute(w, data)
+	check(err)
+
+	// 定义Items为空的数据
+	noItems := struct {
+		Title string
+		Items []string
+	}{
+		Title: "My another page",
+		Items: []string{},
+	}
+	err = t.Execute(w, noItems)
+	check(err)
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
@@ -38,17 +94,18 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 
 func queryPage(w http.ResponseWriter, r *http.Request) {
 
-	t, _ := template.ParseFiles("templates/index.html", "templates/table.html")
-	t.ExecuteTemplate(w, "index", "")
+	//t, _ := template.ParseFiles("templates/index.html", "templates/table.html")
+	//t.ExecuteTemplate(w, "index", "")
 	// t.Execute(w, "目前未有查询数据展示...")
+	t, _ := template.ParseFiles("templates/index.html", "templates/table.html")
+
 	v := r.FormValue("MACID")
+
 	if v != "" {
 		// 取消正则表达式中对XX-XX-XX-XX-XX-XX的匹配,因为懒不想做转换匹配数据库中的内容
-		if m, _ := regexp.MatchString("^([A-Fa-f0-9]{2}:{5}[A-Fa-f0-9]{2}", v); !m {
-			fmt.Println("User input is not MAC")
-			t.ExecuteTemplate(w, "index",
-				"<h2>请输入正确合规的查询内容.</h2><br><div class=\"prompt\">cmd命令行中输入ipconfig即可看到网卡的正确mac信息.</div>")
-			//fmt.Fprintf(w, "")
+		if m, _ := regexp.MatchString("^([A-Fa-f0-9]{2}:){5}[A-Fa-f0-9]{2}", v); !m {
+			// fmt.Println("User input is not MAC")
+			t.ExecuteTemplate(w, "index", "请输入正确的mac格式...cmd命令行中输入ipconfig即可看到网卡的正确mac信息")
 			return
 		}
 
@@ -56,9 +113,9 @@ func queryPage(w http.ResponseWriter, r *http.Request) {
 		t.ExecuteTemplate(w, "table", "")
 		return
 
+	} else {
+		t.ExecuteTemplate(w, "index", "请输入查询内容")
 	}
-	t.ExecuteTemplate(w, "index", "<h2>请输入查询内容.</h2>")
-	// fmt.Fprintf(w, "<h2>请输入查询内容.</h2>")
 
 }
 
