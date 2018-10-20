cfb2673a8dda24054e42a4be842c34a7cb1e2fa0
Oguangpan <panndora@deadpig.cc>
2018-10-20 15:50:07 +0800

    In continuous improvement, the query page is completed

--- main.go
+++ main.go
@@ -16,37 +16,40 @@ func init() {
 	db, _ = sql.Open("sqlite3", "data/szdevDB.db")
 }
 
-type diannao struct {
-	shuxing map[string]string
+type Basic_information_of_device struct {
+	User string
+	Dep  string
+	Type string
+	Mac  string
+	Ip   string
+	Sys  string
+	Disk string
+	Id   string
 }
 
-//var d = new(diannao)
-//d.chaxun("92:4f:9a:ec:78:fd")
-//d.genggai("92:4f:9a:ec:78:fd")
-//d.shanchu("92:4f:9a:ec:78:fd")
-//d.charu(map[string]string)
+var d *Basic_information_of_device = new(Basic_information_of_device)
 
-// 查询数据
-func (s *diannao) chaxun(mac string) (ok bool) {
+func (s *Basic_information_of_device) Inquire(mac string) (ok bool) {
 	var t string = `select * from COMPUTERA where MAC='` + mac + `'`
 	row := db.QueryRow(t)
-	err := row.Scan(&p["id"], &p["user"], &p["dep"], &p["type"], &p["sys"], &p["ip"], &p["mac"], &p["disk"])
+	err := row.Scan(&s.Id, &s.User, &s.Dep, &s.Type, &s.Sys, &s.Ip, &s.Mac, &s.Disk)
 	if err != nil {
 		return false
 	}
 	return true
 }
-func (s *diannao) genggai(mac string) (ok bool) {
 
-	stmt, err := db.Prepare("update COMPUTERA set ID=?,USER=?,DEP=?,TYPE=?,SYS=?,IP=?,MAC=?,DISK=? where MAC=?")
-	if err != nil {
-		return false
-	}
-	stmt.Exec(s.shuxing["id"], s.shuxing["user"], s.shuxing["dep"], s.shuxing["type"], s.shuxing["sys"], s.shuxing["ip"], s.shuxing["mac"], s.shuxing["disk"], mac)
-	return true
+//func (s *Basic_information_of_device) Modify(mac string) (ok bool) {
 
-}
-func (s *diannao) shanchu(mac string) (ok bool) {
+//	stmt, err := db.Prepare("update COMPUTERA set ID=?,USER=?,DEP=?,TYPE=?,SYS=?,IP=?,MAC=?,DISK=? where MAC=?")
+//	if err != nil {
+//		return false
+//	}
+//	stmt.Exec(s.Attributes["id"], s.Attributes["user"], s.Attributes["dep"], s.Attributes["type"], s.Attributes["sys"], s.Attributes["ip"], s.Attributes["mac"], s.Attributes["disk"], mac)
+//	return true
+
+//}
+func (s *Basic_information_of_device) Delete(mac string) (ok bool) {
 	stmt, err := db.Prepare("delete from COMPUTERA where MAC=?")
 	if err != nil {
 		return false
@@ -57,14 +60,22 @@ func (s *diannao) shanchu(mac string) (ok bool) {
 	}
 	return true
 }
-func (s *diannao) charu() (ok bool) {
-	sql := `INSERT INTO COMPUTERA VALUES ('` + s.shuxing["id"] + `','` + s.shuxing["user"] + `','` + s.shuxing["dep"] + `','` + s.shuxing["type"] + `','` + s.shuxing["sys"] + `','` + s.shuxing["ip"] + `','` + s.shuxing["mac"] + `','` + s.shuxing["disk"] + `');`
-	db.Exec(sql)
-	return true
-}
+
+//func (s *Basic_information_of_device) Increase() (ok bool) {
+//	sql := `INSERT INTO COMPUTERA VALUES ('` + s.Attributes["id"] + `','` + s.Attributes["user"] + `','` + s.Attributes["dep"] + `','` + s.Attributes["type"] + `','` + s.Attributes["sys"] + `','` + s.Attributes["ip"] + `','` + s.Attributes["mac"] + `','` + s.Attributes["disk"] + `');`
+//	db.Exec(sql)
+//	return true
+//}
 
 // 路由
 type MyMux struct{}
+
+type hd struct {
+	Msg  string
+	Data *Basic_information_of_device
+}
+
+var h = new(hd)
 
 func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
@@ -86,15 +97,17 @@ func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func mainPage(w http.ResponseWriter, r *http.Request) {
 
 	t, _ := template.ParseFiles("templates/index.html", "templates/table.html", "templates/head.html", "templates/tail.html")
-	*hd.Msg = "欢迎使用办公设备信息查询系统"
-	//	data := struct {
-	//		Msg  string
-	//		Data []string
-	//	}{
-	//		Msg:  "欢迎使用办公设备信息查询系统",
-	//		Data: []string{},
-	//	}
-	t.ExecuteTemplate(w, "index", *hd)
+	h.Msg = "无权限限制请随意更改数据库"
+	//h.Data = d
+	h.Data.User = ""
+	h.Data.Dep = ""
+	h.Data.Type = ""
+	h.Data.Mac = ""
+	h.Data.Ip = ""
+	h.Data.Sys = ""
+	h.Data.Disk = ""
+	h.Data.Id = ""
+	t.ExecuteTemplate(w, "index", h)
 
 }
 
@@ -106,51 +119,28 @@ func queryPage(w http.ResponseWriter, r *http.Request) {
 
 	if v != "" {
 		if m, _ := regexp.MatchString("^([A-Fa-f0-9]{2}:){5}[A-Fa-f0-9]{2}", v); !m {
-			//			data := struct {
-			//				Msg  string
-			//				Data []string
-			//			}{
-			//				Msg:  "请输入正确的mac格式...cmd命令行中输入ipconfig即可看到网卡的正确mac信息",
-			//				Data: []string{},
-			//			}
-			*hd.Msg = "请输入正确的mac格式...cmd命令行中输入ipconfig即可看到网卡的正确mac信息"
-			t.ExecuteTemplate(w, "index", *hd)
+			h.Data = d
+			h.Msg = "请输入正确的mac格式...cmd命令行中输入ipconfig即可看到网卡的正确mac信息"
+			t.ExecuteTemplate(w, "index", h)
 		} else {
 			// 调用数据库查询 v 返回对应数据到 Data 里面
-			//			data := struct {
-			//				Msg  string
-			//				Data []string
-			//			}{
-			//				Msg:  "查询结果如下",
-			//				Data: []string{},
-			//			}
-			*hd.Msg = "查询结果如下"
+			if ok := d.Inquire(v); ok {
+				h.Msg = "查询结果如下"
+				h.Data = d
+				t.ExecuteTemplate(w, "index", h)
+			} else {
+				h.Msg = "数据库中没有该设备信息"
+				t.ExecuteTemplate(w, "index", h)
+			}
 
-			/*
-					<tr>
-				        <td>使用者姓名</td>
-				        <td>张三</td>
-				    </tr>
-				    <tr class="alt">
-				        <td>所属部门</td>
-				        <td>生产安全部</td>
-				    </tr>
-			*/
-			t.ExecuteTemplate(w, "index", *hd)
 		}
 
 		return
 
 	} else {
-		//		data := struct {
-		//			Msg  string
-		//			Data []string
-		//		}{
-		//			Msg:  "请输入查询内容",
-		//			Data: []string{},
-		//		}
-		*hd.Msg = "请输入查询内容"
-		t.ExecuteTemplate(w, "index", hd)
+		h.Msg = "请输入查询内容"
+		h.Data = d
+		t.ExecuteTemplate(w, "index", h)
 	}
 
 }
@@ -162,7 +152,7 @@ func editPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
+	defer db.Close()
 	// 特别说明: 要使用80端口需要使用管理员身份运行程序.
 	mux := &MyMux{}
 	fmt.Println("now Listening port...")
